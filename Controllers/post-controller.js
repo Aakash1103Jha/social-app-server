39261ae23065cc3a9ec418de622c9b319ff4b509
Aakash Jha <aakash.jha1103@gmail.com>
2021-11-23 11:26:45 +0530

    - added multer config for post upload - file data received as req

--- Controllers/post-controller.js
+++ Controllers/post-controller.js
@@ -10,22 +10,25 @@ exports.getAllPosts = async (req, res) => {
 }
 
 exports.newPostByUser = async (req, res) => {
-	if (!req.body || !req.params) {
-		return res.status(404).json("Something went wrong...")
-	}
-	const username = req.params.username
-	const newPost = new Post({
-		content: req.body.content,
-		hashtags: req.body.hashtags,
-		imageUrl: req.body.imageUrl,
-		_username: username,
-	})
-	try {
-		const savedPost = await newPost.save()
-		return res.status(200).json(savedPost)
-	} catch (error) {
-		return res.status(404).json(error.message)
-	}
+	// if (!req.body || !req.params || !req.file) {
+	// 	return res.status(404).json("Something went wrong...")
+	// }
+	console.log({ body: req.body })
+	console.log({ params: req.params })
+	console.log({ file: req.file })
+	// const username = req.params.username
+	// const newPost = new Post({
+	// 	content: req.body.content,
+	// 	hashtags: req.body.hashtags,
+	// 	imageUrl: req.body.imageUrl,
+	// 	_username: username,
+	// })
+	// try {
+	// 	const savedPost = await newPost.save()
+	// 	return res.status(200).json(savedPost)
+	// } catch (error) {
+	// 	return res.status(404).json(error.message)
+	// }
 }
 
 exports.deleteOnePostById = async (req, res) => {}
