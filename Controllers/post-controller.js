2a1ecd54d5e2922d924269b07328cf8f1f68e427
Aakash Jha <aakash.jha1103@gmail.com>
2021-11-23 17:50:55 +0530

    - add new post - delete a post

--- Controllers/post-controller.js
+++ Controllers/post-controller.js
@@ -10,28 +10,42 @@ exports.getAllPosts = async (req, res) => {
 }
 
 exports.newPostByUser = async (req, res) => {
-	// if (!req.body || !req.params || !req.file) {
-	// 	return res.status(404).json("Something went wrong...")
-	// }
-	console.log({ body: req.body })
-	console.log({ params: req.params })
-	console.log({ file: req.file })
-	// const username = req.params.username
-	// const newPost = new Post({
-	// 	content: req.body.content,
-	// 	hashtags: req.body.hashtags,
-	// 	imageUrl: req.body.imageUrl,
-	// 	_username: username,
-	// })
-	// try {
-	// 	const savedPost = await newPost.save()
-	// 	return res.status(200).json(savedPost)
-	// } catch (error) {
-	// 	return res.status(404).json(error.message)
-	// }
+	if (!req.body || !req.params || !req.file) {
+		return res.status(404).json("Something went wrong...")
+	}
+	// console.log({ body: req.body })
+	// console.log({ params: req.params })
+	// console.log({ file: req.file })
+
+	const username = req.params.username
+
+	const contentArray = req.body.content.split(/\r/)
+
+	const hashtagArray = req.body.hashtags.split(/\s/)
+
+	const newPost = new Post({
+		content: contentArray,
+		hashtags: hashtagArray,
+		imageUrl: req.file.filename,
+		_username: username,
+	})
+	// console.log(newPost)
+	try {
+		const savedPost = await newPost.save()
+		return res.status(200).json(savedPost)
+	} catch (error) {
+		return res.status(404).json(error.message)
+	}
 }
 
-exports.deleteOnePostById = async (req, res) => {}
+exports.deleteOnePostById = async (req, res) => {
+	try {
+		const deletePost = await Post.findByIdAndDelete({ _id: req.params.postId })
+		return res.status(200).json(deletePost)
+	} catch (error) {
+		return res.status(404).json(error.message)
+	}
+}
 
 exports.likeOnePostById = async (req, res) => {}
 
